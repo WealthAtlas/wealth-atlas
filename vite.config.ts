b7bb306d1ee7aa6af4e555d98e50b285054fc196
Prasanna Anbazhagan <prasannaa@MacBookPro.communityfibre.co.uk>
2025-09-06 09:20:30 +0100

    Fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,42 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+            },
+          },
+        ],
+      },
+      includeAssets: ['favicon.ico', 'icons/*.png'],
+      manifest: {
+        name: 'Wealth Atlas',
+        short_name: 'WealthAtlas',
+        description: 'Personal wealth management PWA',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
